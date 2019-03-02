d366cf7c1643c8c63dcf18f5683283040fe9d5de
17092315 <Lepa.I.D@sberbank.ru>
2019-03-02 13:18:50 +0300

    problem

--- src/main/java/entity/PassportToIdConstants.java
+++ src/main/java/entity/PassportToIdConstants.java
@@ -4,7 +4,7 @@ import org.springframework.security.core.parameters.P;
 
 public class PassportToIdConstants {
 
-    public final static int[] WORKOUT = {898};
+    public final static int[] WORKOUT = {893, 898, 1251};
     public final static int[] AUTODROM = {1384, 1385};
     public final static int[] SKISLOPES = {1244};
 
@@ -13,7 +13,26 @@ public class PassportToIdConstants {
     public final static int[] SPORTS_HALLS = {60622};
     public final static int[] TENNIS_HALLS = {60623};
     public final static int[] TRAINING_HALLS = {60624};
-    public final static int[]
+    public final static int[] ICEFIELD = {1232};
+    public final static int[] ICESLIDEINPARKS = {1245};
+    public final static int[] SKIWAYS = {1233, 1246};
+    public final static int[] HORSE_BACKRIDING = {1386};
+    public final static int[] VENUES = {917};
+    public final static int[] CURLING = {1249};
+    public final static int[] GROUNDS_FOR_SNOWBOARDING = {1250};
+    public final static int[] PAINTBALL = {895};
+    public final static int[] HORSE_FIELDS = {902};
+    public final static int[] RUGBY = {885};
+    public final static int[] FOOTBALL = {886};
+    public final static int[] WINTER_BATHING = {1247};
+    public final static int[] SNOWY_TOWNS = {};
+    public final static int[] DANCE = {2115};
+    public final static int[] SNOW_MOBILES = {1254};
+    public final static int[] CLIMBING_WALL = {1387};
+    public final static int[] ICERINKS = {1231};
+    public final static int[] PARKLAND = {1465};
+    public final static int[] PEDESTRIAN_AREAS = {920};
+
 
 
 }
