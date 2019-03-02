0de25f0b94b5d3cb7f9de73967ce2dff0ed5d48a
kirillovcinnikov <kirock.98@mail.ru>
2019-03-02 11:04:56 +0300

    Update Pom

---
+++ src/main/java/endpoint/CarpeDiem.java
@@ -0,0 +1,13 @@
+package endpoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarpeDiem {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarpeDiem.class, args);
+    }
+
+}
--- src/main/java/endpoint/caprediem.java
+++
@@ -1,8 +0,0 @@
-package endpoint;
-
-public class caprediem {
-
-    public static void main(String[] args) {
-        SpringApplication.run(caprediem.class, args);
-    }
-}
