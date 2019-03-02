77399b8756285a13ccb5aa7707f72f747adb3dbb
17092315 <Lepa.I.D@sberbank.ru>
2019-03-02 12:57:23 +0300

    fix

---
+++ src/main/java/entity/PassportToIdConstants.java
@@ -0,0 +1,19 @@
+package entity;
+
+import org.springframework.security.core.parameters.P;
+
+public class PassportToIdConstants {
+
+    public final static int[] WORKOUT = {898};
+    public final static int[] AUTODROM = {1384, 1385};
+    public final static int[] SKISLOPES = {1244};
+
+    public final static int[] SWIMMING_POOL = {890, 894};
+    public final static int[] PLAYGROUND = {1389};
+    public final static int[] SPORTS_HALLS = {60622};
+    public final static int[] TENNIS_HALLS = {60623};
+    public final static int[] TRAINING_HALLS = {60624};
+    public final static int[]
+
+
+}
